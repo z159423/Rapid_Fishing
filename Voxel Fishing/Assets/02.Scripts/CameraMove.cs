25fd36b9e72c571f8bfafe0590357cbcd4346688
방승환 <tmdghks992@naver.com>
2022-08-15 16:17:08 +0900

    revert

--- Voxel Fishing/Assets/02.Scripts/CameraMove.cs
+++ Voxel Fishing/Assets/02.Scripts/CameraMove.cs
@@ -13,6 +13,5 @@ public class CameraMove : MonoBehaviour
 
         newCamera.transform.position = new Vector3(target.transform.position.x, Vector3.Slerp(newCamera.transform.position, target.transform.position, 1 * Time.deltaTime).y, newCamera.transform.position.z);
         
-        //Test
     }
 }
